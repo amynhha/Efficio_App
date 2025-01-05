b83a8f3b0933573c369fb52135ed4941740a629a
Amy <honganh171105@gmail.com>
2025-01-05 15:42:18 -0600

    checking host running

--- databse.js
+++ databse.js
@@ -1,4 +1,11 @@
-const { 
-    createPool
+// Use mysql2 library and dotenv
+const mysql = require('mysql2');
+require('dotenv').config();
 
-} = require('dotenv').config();
+// Create a connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,      
+  user: process.env.DB_USER,      
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME    
+});
